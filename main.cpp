18be2b5feb9a340cc4949e5cdc8c480fbde07a3a
Amirreza Ipchi-e-Haq <99384891+Amirreza-Ipchi-Haq@users.noreply.github.com>
2023-06-01 20:45:33 +0330

    Add the pipe & interactive-mode features

--- main.cpp
+++ main.cpp
@@ -1,74 +1,173 @@
-#include<conio.h>
-#include<deque>
 #include<cstdio>
+#include<deque>
 #include<string>
+#ifdef _WIN32
+#include<io.h>
+#define isatty _isatty
+#else
+#include<unistd.h>
+#endif
 int main(int argc,char** argv){
-	std::string code;
-	std::deque<char>pointer={0};
-	FILE *file=fopen(argv[1],"r");
-	for(auto command=getc(file);!feof(file);command=getc(file))
-		switch(command){
-			case '>':
-			case '<':
-			case '+':
-			case '-':
-			case '.':
-			case ',':
-			case '[':
-			case ']':
-				code+=command;
+	std::string code;//Initialize the main code
+	std::deque<char>pointer={0};//Initialize pointers
+	FILE *input;//Initialize input for pipes
+	if(argc>1){//Read from a file if argument is given
+		FILE *file=fopen(argv[1],"r");//Open the file
+		for(char command=getc(file);!feof(file);command=getc(file))//Read each character of the file
+			switch(command){//Only store the commands acceptable for the language
+				case '>':
+				case '<':
+				case '+':
+				case '-':
+				case '.':
+				case ',':
+				case '[':
+				case ']':
+					code+=command;
+			}
+		fclose(file);//Close the file
+	}else if(!isatty(0)){//Read from a pipe
+		input=fopen("/dev/tty","r");//Open input
+		if(input==NULL){//Send an error if the current operating system is Windows
+			fprintf(stderr,"Error!\n");
+			return 1;
 		}
-	fclose(file);
-	for(size_t codeLocation=0,pointerLocation=0;codeLocation<code.size();codeLocation++)
-		switch(code[codeLocation]){
-			case '>':
-				if(++pointerLocation==pointer.size())
+		for(char command=getchar();!feof(stdin);command=getchar())//Read each character of the file
+			switch(command){//Only store the commands acceptable for the language
+				case '>':
+				case '<':
+				case '+':
+				case '-':
+				case '.':
+				case ',':
+				case '[':
+				case ']':
+					code+=command;
+			}
+	}else{//(Interactive mode)
+		printf("Entered interactive-mode!\n");//Notify
+		for(size_t pointerLocation=0;1;code=""){//Read a line each time
+			printf(">>> ");//Prompt
+			for(char command=getchar();command!='\n';command=getchar())//Read from the input
+				switch(command){//Only store the commands acceptable for the language
+					case '>':
+					case '<':
+					case '+':
+					case '-':
+					case '.':
+					case ',':
+					case '[':
+					case ']':
+						code+=command;
+				}
+			for(size_t codeLocation=0;codeLocation<code.size();codeLocation++)//Read a part of code each time
+				switch(code[codeLocation]){//Choose a command based on the code
+					case '>'://Push the pointer indicator forward
+						if(++pointerLocation==pointer.size())//Add a new pointer to the back if needed
+							pointer.push_back(0);
+						break;
+					case '<'://Push the pointer indicator backward
+						if(!pointerLocation--)//Add a new pointer to the front if needed
+							pointer.push_front(0),pointerLocation++;
+						break;
+					case '+'://Increase the current pointer value by 1
+						pointer[pointerLocation]++;
+						break;
+					case '-'://Decrease the currnet pointer value by 1
+						pointer[pointerLocation]--;
+						break;
+					case '.'://Output the current pointer as a character
+						putchar(pointer[pointerLocation]);
+						break;
+					case ','://Input a character to the current pointer
+						pointer[pointerLocation]=getchar();
+						getchar();//Skip the newline
+						break;
+					case '['://Skip to a matching `]` if the current pointer value is 0
+						if(!pointer[pointerLocation]){
+							auto debug=codeLocation;//Save current pointer location for debug purposes
+							codeLocation++;//Increase the pointer indicator value
+							for(unsigned long long i=1;i;codeLocation++)//Skip part of the code until reaching a matching `]`
+								if(codeLocation+1==code.size()&&(code[codeLocation]!=']'||i>1)){//Alert and abort the program if there's no matching `]`
+									fprintf(stderr,"Error at locaton %u: Missing ']'!\n",debug);
+									return 2;
+								}else if(code[codeLocation]=='[')//(Nested `[`)
+									i++;
+								else if(code[codeLocation]==']')//(Matching `]`)
+									i--;
+							codeLocation--;//Decrease the pointer indicator value
+						}
+						break;
+					case ']'://Skip to a matching `]` if the current pointer value is non-zero
+						if(pointer[pointerLocation]){
+							auto debug=codeLocation;//Save current pointer location for debug purposes
+							codeLocation--;//Decrease the pointer indicator value
+							for(unsigned long long i=1;i;codeLocation--)//Go back until reaching a matching `[`
+								if(!codeLocation&&(code[codeLocation]!='['||i>1)){//Alert and abort the program if there's no matching `[`
+									fprintf(stderr,"Error at locaton %u: Missing '['!\n",debug);
+									return 2;
+								}else if(code[codeLocation]=='[')//(Nested `[`)
+									i--;
+								else if(code[codeLocation]==']')//(Matching `]`)
+									i++;
+							codeLocation++;//Increase the pointer indicator value
+						}
+				}
+		}
+	}
+	for(size_t codeLocation=0,pointerLocation=0;codeLocation<code.size();codeLocation++)//Read a part of code each time
+		switch(code[codeLocation]){//Choose a command based on the code
+			case '>'://Push the pointer indicator forward
+				if(++pointerLocation==pointer.size())//Add a new pointer to the back if needed
 					pointer.push_back(0);
 				break;
-			case '<':
-				if(!pointerLocation--)
+			case '<'://Push the pointer indicator backward
+				if(!pointerLocation--)//Add a new pointer to the front if needed
 					pointer.push_front(0),pointerLocation++;
 				break;
-			case '+':
+			case '+'://Increase the current pointer value by 1
 				pointer[pointerLocation]++;
 				break;
-			case '-':
+			case '-'://Decrease the currnet pointer value by 1
 				pointer[pointerLocation]--;
 				break;
-			case '.':
+			case '.'://Output the current pointer as a character
 				putchar(pointer[pointerLocation]);
 				break;
-			case ',':
-				pointer[pointerLocation]=getche();
+			case ','://Input a character to the current pointer
+				if(isatty(0))
+					pointer[pointerLocation]=getc(input);
+				else
+					pointer[pointerLocation]=getchar();
 				break;
-			case '[':
+			case '['://Skip to a matching `]` if the current pointer value is 0
 				if(!pointer[pointerLocation]){
-					auto debug=codeLocation;
-					codeLocation++;
-					for(unsigned long long i=1;i;codeLocation++)
-						if(codeLocation+1==code.size()&&(code[codeLocation]!=']'||i>1)){
+					auto debug=codeLocation;//Save current pointer location for debug purposes
+					codeLocation++;//Increase the pointer indicator value
+					for(unsigned long long i=1;i;codeLocation++)//Skip part of the code until reaching a matching `]`
+						if(codeLocation+1==code.size()&&(code[codeLocation]!=']'||i>1)){//Alert and abort the program if there's no matching `]`
 							fprintf(stderr,"Error at locaton %u: Missing ']'!\n",debug);
-							return 1;
-						}else if(code[codeLocation]=='[')
+							return 2;
+						}else if(code[codeLocation]=='[')//(Nested `[`)
 							i++;
-						else if(code[codeLocation]==']')
+						else if(code[codeLocation]==']')//(Matching `]`)
 							i--;
-					codeLocation--;
+					codeLocation--;//Decrease the pointer indicator value
 				}
 				break;
-			case ']':
+			case ']'://Skip to a matching `]` if the current pointer value is non-zero
 				if(pointer[pointerLocation]){
-					auto debug=codeLocation;
-					codeLocation--;
-					for(unsigned long long i=1;i;codeLocation--)
-						if(!codeLocation&&(code[codeLocation]!='['||i>1)){
+					auto debug=codeLocation;//Save current pointer location for debug purposes
+					codeLocation--;//Decrease the pointer indicator value
+					for(unsigned long long i=1;i;codeLocation--)//Go back until reaching a matching `[`
+						if(!codeLocation&&(code[codeLocation]!='['||i>1)){//Alert and abort the program if there's no matching `[`
 							fprintf(stderr,"Error at locaton %u: Missing '['!\n",debug);
-							return 1;
-						}else if(code[codeLocation]=='[')
+							return 2;
+						}else if(code[codeLocation]=='[')//(Nested `[`)
 							i--;
-						else if(code[codeLocation]==']')
+						else if(code[codeLocation]==']')//(Matching `]`)
 							i++;
-					codeLocation++;
+					codeLocation++;//Increase the pointer indicator value
 				}
 		}
 	return 0;
